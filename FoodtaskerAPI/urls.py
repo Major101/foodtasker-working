0cc60146cc0bca7ffeef894eb8131df01eed7c33
Major101 <major101@gmail.com>
2019-04-26 14:10:34 +0500

    User and Restaurant Registration

--- FoodtaskerAPI/urls.py
+++ FoodtaskerAPI/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from foodtaskerapp import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-]
+    # Sign in / Sign out /  Sign up
+    path('restaurant/sign-in/', auth_views.LoginView.as_view(template_name='restaurant/sign_in.html'),
+         name='restaurant-sign-in'),
+    path('restaurant/sign-up', views.restaurant_sign_up, name='restaurant-sign-up'),
+    path('restaurant/sign-out/', auth_views.LogoutView.as_view(next_page='/'), name='restaurant-sign-out'),
+    # Home Page
+    path('restaurant/', views.restaurant_home, name='restaurant-home'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
